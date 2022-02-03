8a08b9c330705cb4c9b017b253241c3cc44a018c
madeinkgb@gmail.com <madeinkgb@gmail.com>
2022-02-03 10:19:05 +0900

    sec commit

--- Watcher/src/main/java/kr/uracle/ums/monit/config/DatasourceConfig.java
+++ Watcher/src/main/java/kr/uracle/ums/monit/config/DatasourceConfig.java
@@ -1,6 +1,11 @@
 package kr.uracle.ums.monit.config;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -9,6 +14,8 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,6 +23,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.jolbox.bonecp.BoneCPDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import kr.uracle.ums.monit.mng.EncrytManager;
 
 
 @ComponentScan(basePackages = {"kr.uracle.ums.monit"},useDefaultFilters = true)
@@ -23,6 +34,11 @@ import com.jolbox.bonecp.BoneCPDataSource;
 public class DatasourceConfig {
 
     private Logger log = LoggerFactory.getLogger("Developer");
+    
+    private final String CONFIG1 = "db1.properties";
+    
+    @Autowired
+    EncrytManager encrytManager;
     
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
@@ -34,8 +50,8 @@ public class DatasourceConfig {
     private List<String> password;
 
 
-    @Bean(name="dataSorce")
-    public DataSource dataSource() {
+   
+    public DataSource boneCpDataSource() {
         log.info("###[DB] dataSource load start");
         BoneCPDataSource ds = new BoneCPDataSource();
         ds.setDriverClass(driverClassName);
@@ -48,26 +64,61 @@ public class DatasourceConfig {
         ds.setConnectionTestStatement("select 1");
         ds.setIdleConnectionTestPeriodInMinutes(5);
         ds.setDisableConnectionTracking(true);
-        log.info("###[DB] dataSource completed");
+        log.info("###[DB] bonecp dataSource created");
         return ds;
     }
 
-    @Bean(name="sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        log.info("###[DB] sqlSessionFactory start");
+    
+    public DataSource hikariDataSource() throws FileNotFoundException, IOException {
+    	HikariConfig config = new HikariConfig(getProperties(CONFIG1));
+    	HikariDataSource dataSource = new HikariDataSource(config);
+    	log.info("###[DB] hikari dataSource created");
+    	return dataSource;
+    }
+    
+    @Bean(name="boneCpSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory1() throws Exception {
         String mapperSrc= "classpath:mybatis/**/*.xml";
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
+        bean.setDataSource(boneCpDataSource());
         bean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:config/mybatis-config.xml"));
         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperSrc));
-        log.info("###[DB] sqlSessionFactory completed");
+        log.info("###[DB] bonecp sqlSessionFactory created");
+        return bean.getObject();
+    }
+    
+    @Bean(name="hikariCpSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory2() throws Exception {
+        String mapperSrc= "classpath:mybatis/**/*.xml";
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(hikariDataSource());
+        bean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:config/mybatis-config.xml"));
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperSrc));
+        log.info("###[DB] hikari sqlSessionFactory created");
         return bean.getObject();
     }
 
-    @Bean(name="sessionTemplate")
-    public SqlSessionTemplate sqlSession(DataSource dataSource) throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory(dataSource));
+    @Bean(name="bonSessionTemplate")
+    public SqlSessionTemplate sqlSession1(@Qualifier("boneCpSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+    
+    @Bean(name="hikariSessionTemplate")
+    public SqlSessionTemplate sqlSession2(@Qualifier("hikariCpSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 
-
+    
+    private Properties getProperties(String path) throws FileNotFoundException, IOException {
+    	Properties pro = new Properties();
+    	String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    	String appConfigPath = rootPath + CONFIG1;
+    	pro.load(new FileInputStream(appConfigPath));
+    	
+    	for(Entry<Object, Object> s:pro.entrySet()) {
+    		String value = s.getValue().toString();
+        	if(value.startsWith("$ENC:")) pro.setProperty(s.getKey().toString(), encrytManager.decrypt(value.replace("$ENC:", "")));
+    	}
+    	return pro;
+    }
 }
---
+++ Watcher/src/main/java/kr/uracle/ums/monit/mng/EncrytManager.java
@@ -0,0 +1,57 @@
+package kr.uracle.ums.monit.mng;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import kr.msp.util.AES128Cipher;
+
+@Component
+public class EncrytManager {
+	
+	private static final Logger log = LoggerFactory.getLogger(EncrytManager.class);
+	
+	private final static AES128Cipher tool =  AES128Cipher.getInstance();
+	
+	public static String encrypt(String plainText) {
+		String encryptText = "";
+		if(StringUtils.isNotEmpty(plainText)) {
+			try {
+				encryptText = tool.AES_Encode(plainText);
+			} catch (Exception e) {
+				log.error("{} 문자열 암호화중 에러 발생", plainText);
+				return encryptText;
+			}
+		}
+		return encryptText;
+	}
+	
+	public static String decrypt(String encryptText) {
+		String decryptText ="";
+		if(StringUtils.isNotEmpty(encryptText)) {
+			try {
+				decryptText = tool.AES_Decode(encryptText);
+			} catch (Exception e) {
+				log.error("{} 문자열 암호화중 에러 발생", encryptText);
+				return decryptText;
+			}
+		}
+		return decryptText;
+	}
+	
+	public static void main(String[] args) {
+		if(args.length <2) {
+			log.error("암/복호화할 데이터(2개)를 입력바랍니다. - 타입 암보호화문자열");
+			System.exit(-1);
+		}
+		String type = args[0];
+		if(type.equals("ENC")||type.equals("DEC")) {
+			if(type.equals("ENC")) log.info("암호화 데이터 - [{}]", encrypt(args[1]));
+			if(type.equals("DEC")) log.info(" 복호화 데이터 - [{}]", decrypt(args[1]));
+		}else {
+			log.error("첫번째({}) 타입 지정 값이 올바르지 않습니다.(ENC/DEC 지원)", type);
+			System.exit(-1);
+		}
+	}
+}
